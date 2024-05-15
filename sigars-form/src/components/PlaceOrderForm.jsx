e49b7a043768c1b0f05e9562d0920145d9ee9a56
Volodymyr Lysenko <lusiy321@gmail.com>
2024-05-15 12:28:05 +0300

    user id

--- sigars-form/src/components/PlaceOrderForm.jsx
+++ sigars-form/src/components/PlaceOrderForm.jsx
@@ -26,7 +26,7 @@ function PlaceOrderForm({ products, onSubmit }) {
         style={{ width: "300px", margin: "100px auto" }}
       >
         <h1>Замовлення</h1>
-        <p>User id: {tg.initDataUnsafe.user.username}</p>
+        <p>User id: {tg.initDataUnsafe?.user?.id}</p>
         <div style={{ marginBottom: "10px" }}>
           <select
             name="product"
